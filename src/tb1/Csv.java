1e56ad54e94f86f58979c06a8644724fd45247db
ralf@bensmann.com <ralf@bensmann.com>
2009-09-29 10:50:03 +0200

    First import

---
+++ src/tb1/Csv.java
@@ -0,0 +1,26 @@
+package tb1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author rbe
+ */
+public class Csv {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+//        File csv = new File("c:/tb1.csv");
+//        BufferedReader br = new BufferedReader(new FileReader(csv));
+//        String s = null;
+//        while (null != (s = br.readLine())) {
+//            String[] fields = s.split(";");
+//            for (String field : fields) {
+//                System.out.println(""+field);
+//            }
+//        }
+    }
+}
