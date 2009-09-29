124a075b056ef3b5996ec09767c19640118ba7a4
tim.brandes@sfh-muenster.de <tim.brandes@sfh-muenster.de>
2009-09-29 17:02:59 +0200

    csvDataImporter angepasst PprPlugIn class erstellt

--- src/de/sfhms/skb/input/CsvDataImportAdapterImpl.java
+++ src/de/sfhms/skb/input/CsvDataImportAdapterImpl.java
@@ -1,15 +1,31 @@
 package de.sfhms.skb.input;
 
+import au.com.bytecode.opencsv.CSVReader;
 import de.sfhms.skb.model.MyCell;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CsvDataImportAdapterImpl implements DataImportAdapter {
+
+    CSVReader reader = null;
 
     public CsvDataImportAdapterImpl() {
     }
 
     public void open(URL url) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            reader = new CSVReader(new FileReader(new File(url.toURI())), ';');
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CsvDataImportAdapterImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(CsvDataImportAdapterImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void open(String url) {
@@ -17,7 +33,15 @@ public class CsvDataImportAdapterImpl implements DataImportAdapter {
     }
 
     public void init() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String[] nextLine;
+        try {
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                System.out.println(nextLine.length);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(CsvDataImportAdapterImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public MyCell getCell(int sheet, int row, int column) {
--- src/de/sfhms/skb/processor/plugin/PprPlugIn.java
+++ src/de/sfhms/skb/processor/plugin/PprPlugIn.java
@@ -9,17 +9,17 @@ import java.util.logging.Level;
 
 /**
  *
- * @author rbe
+ * @author brandes
  */
-public class AkvdInputPlugin extends AbstractPlugin {
+public class PprPlugIn extends AbstractPlugin {
 
     @Override
     public void execute() {
         try {
             // Load Excel sheet
-            DataImportAdapter excel = DataImportFactory.createExcelImportAdapter(new URL("file:///c:/test.xls"));
+            DataImportAdapter csv = DataImportFactory.createCsvImportAdapter(new URL("file:///c:/test.csv"));
             // Get cell value
-            System.out.println("" + excel.getCell(0, 0, 0).getValue());
+            //System.out.println("" + csv.getCell(0, 0, 0).getValue());
             // Calculate...
             // Set var
         } catch (MalformedURLException ex) {
@@ -32,9 +32,8 @@ public class AkvdInputPlugin extends AbstractPlugin {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-//    public static void main(String[] args) {
-//        AkvdInputPlugin test = new AkvdInputPlugin();
-//        test.execute();
-//
-//    }
+    public static void main(String[] args) {
+        PprPlugIn test = new PprPlugIn();
+        test.execute();
+    }
 }
